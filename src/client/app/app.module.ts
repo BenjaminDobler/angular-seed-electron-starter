a262ae940693c974c609d8b8079e09357a353ac4
Benjamin Dobler <benz@richapps.de>
2017-03-22 11:26:18 +0100

    added base href setting for electron

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,21 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
-import { HttpModule } from '@angular/http';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {
+  APP_BASE_HREF, HashLocationStrategy, LocationStrategy, PathLocationStrategy,
+  PlatformLocation
+} from '@angular/common';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
 
-import { HomeModule } from './home/home.module';
-import { SharedModule } from './shared/shared.module';
+import {HomeModule} from './home/home.module';
+import {SharedModule} from './shared/shared.module';
+
+
+export function locationFactory(platformLocation: PlatformLocation): LocationStrategy {
+  let w: any = window;
+  if (w && w.process && w.process.type) {
+    return new HashLocationStrategy(platformLocation);
+  }
+  return new PathLocationStrategy(platformLocation);
+}
+
+export function getBaseHref(): String {
+  let w: any = window;
+  if (w && w.process && w.process.type) {
+    return '/';
+  }
+  return '/';
+}
+
+
+
+
 
 @NgModule({
   imports: [BrowserModule, HttpModule, AppRoutingModule, HomeModule, SharedModule.forRoot()],
   declarations: [AppComponent],
   providers: [{
     provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
+    useFactory: getBaseHref
+  }, {
+    provide: LocationStrategy,
+    useFactory: locationFactory,
+    deps: [PlatformLocation]
   }],
   bootstrap: [AppComponent]
 
 })
-export class AppModule { }
+export class AppModule {
+}
